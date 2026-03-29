92cc2280987f7b70cd69aca00589f5fcd82c5ee0
OllyDI <choiseungho11@hanmail.net>
2026-03-29 10:42:13 +0900

    26.03.29

--- itemservice-db/src/main/java/hello/itemservice/repository/jdbcTemplate/JdbcTemplateRepositoryV3.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbcTemplate/JdbcTemplateRepositoryV3.java
@@ -7,56 +7,46 @@ import hello.itemservice.repository.ItemUpdateDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 /**
- * NamedParameterJdbcTemplate
- * SqlParameterSource
- * - BeanPropertySqlParameterSource
- * - MapSqlParameterSource
- * Map
- * 
- * BeanPropertyRowMapper
- *
+ * SimpleJdbcInsert
+ * - save 메서드만 바뀜
  */
 @Slf4j
-public class JdbcTemplateRepositoryV2 implements ItemRepository {
+public class JdbcTemplateRepositoryV3 implements ItemRepository {
 
-    //    private JdbcTemplate template;
     private final NamedParameterJdbcTemplate template;
+    private final SimpleJdbcInsert jdbcInsert;
 
-    public JdbcTemplateRepositoryV2(DataSource dataSource) {
+    public JdbcTemplateRepositoryV3(DataSource dataSource) {
         this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("item")
+                .usingGeneratedKeyColumns("id");
+//                .usingColumns("item_name", "price", "quantity");  // 생략 가능
     }
 
 
     @Override
     public Item save(Item item) {
-        String sql = "insert into item (item_name, price, quantity) values (:itemName, :price, :quantity)";
-
-        // 첫 번째 방식 - Item에 넘어온 이름으로 파라미터 생성 -> 이 방식이 가장 편리함
+        // SimpleJdbcInsert를 사용하면 대부분의 코드 생략 가능
         SqlParameterSource param = new BeanPropertySqlParameterSource(item);
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        template.update(sql, param, keyHolder);
-
-        long key = keyHolder.getKey().longValue();
-        item.setId(key);
+        Number key = jdbcInsert.executeAndReturnKey(param);
+        item.setId(key.longValue());
         return item;
     }
 
