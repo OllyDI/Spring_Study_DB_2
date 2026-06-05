0113212c49fa64cd3cb629eac9e7b4c30af4ff62
OllyDI <choiseungho11@hanmail.net>
2026-06-05 16:48:08 +0900

    26.06.05

--- springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 
 @Slf4j
 @Service
